3f1033cd9a4696a7ebdd2c8cef9b9847fb94483c
zkzhao333 <65621117+zkzhao333@users.noreply.github.com>
2020-06-09 17:53:59 -0700

    added gas indicator

--- GooglePlayPlugins/com.google.play.billing/Samples/trivalKart/Assets/scripts/PlayerController.cs
+++ GooglePlayPlugins/com.google.play.billing/Samples/trivalKart/Assets/scripts/PlayerController.cs
@@ -3,10 +3,24 @@ using UnityEngine.Serialization;
 
 public class PlayerController : MonoBehaviour
 {
+
     public GameObject cam;
     public GameObject tapToDriveText;
     public Animator animator;
     [FormerlySerializedAs("MovementSpeed")] public float force = 6;
+
+    public GameObject gasStatus0;
+    public GameObject gasStatus1;
+    public GameObject gasStatus2;
+    public GameObject gasStatus3;
+    public GameObject gasStatus4;
+    public GameObject gasStatus5;
+
+    public GameObject noGasText;
+
+    private int _gasLevel;
+    private GameObject[] _gasStatus;
+    private int _fullGasLevel;
     private Vector3 _carStartPos;
     private Rigidbody2D _rigidbody2D;
     private static readonly int Speed = Animator.StringToHash("speed");
@@ -15,6 +29,16 @@ public class PlayerController : MonoBehaviour
     { 
         _carStartPos = transform.position;
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        _gasLevel = 5;
+        _fullGasLevel = 5;
+        _gasStatus = new GameObject[_fullGasLevel];
+        _gasStatus[0] = gasStatus0;
+        _gasStatus[1] = gasStatus1;
+        _gasStatus[2] = gasStatus2;
+        _gasStatus[3] = gasStatus3;
+        _gasStatus[4] = gasStatus4;
+        _gasStatus[5] = gasStatus5;
+        
     }
 
     // Update is called once per frame
@@ -23,7 +47,19 @@ public class PlayerController : MonoBehaviour
         
         if (Input.GetMouseButtonUp(0))
         {
-            Drive();
+            if (_gasLevel > 0)
+            {
+                Drive();
+                _gasLevel--;
+                for (var i = 0; i < _fullGasLevel; i++)
+                {
+                    _gasStatus[i].SetActive(i == _gasLevel);
+                }
+            }
+            else
+            {
+                noGasText.SetActive(true);
+            }
         }
         animator.SetFloat(Speed,  _rigidbody2D.velocity.magnitude);
     }
