2f90f460791f80a649bcd804c4a9e49b976e898a
zkzhao333 <65621117+zkzhao333@users.noreply.github.com>
2020-06-10 14:25:00 -0700

    completed basic game logic

---
+++ GooglePlayPlugins/com.google.play.billing/Samples/trivalKart/Assets/scripts/Gas.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using UnityEngine;
+
+public class Gas : MonoBehaviour
+{
+    public GameObject gasStatus0;
+    public GameObject gasStatus1;
+    public GameObject gasStatus2;
+    public GameObject gasStatus3;
+    public GameObject gasStatus4;
+    public GameObject gasStatus5;
+    public GameObject noGasText;
+    
+    private int _gasLevel;
+    private GameObject[] _gasStatus;
+    private int _fullGasLevel;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _gasLevel = 5;
+        _fullGasLevel = 5;
+        _gasStatus = new GameObject[_fullGasLevel + 1];
+        _gasStatus[0] = gasStatus0;
+        _gasStatus[1] = gasStatus1;
+        _gasStatus[2] = gasStatus2;
+        _gasStatus[3] = gasStatus3;
+        _gasStatus[4] = gasStatus4;
+        _gasStatus[5] = gasStatus5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public int GetGasLevel()
+    {
+        return _gasLevel;
+    }
+
+    public bool ConsumeGas()
+    {
+        if (_gasLevel > 0)
+        {
+            _gasLevel -= 1;
+            for (var i = 0; i <= _fullGasLevel; i++)
+            {
+                _gasStatus[i].SetActive(i == _gasLevel);
+            }
+            return true;
+        }
+        else
+        {
+            noGasText.SetActive(true);
+            return false;
+        }
+       
+    }
+}
--- GooglePlayPlugins/com.google.play.billing/Samples/trivalKart/Assets/scripts/PlayerController.cs
+++ GooglePlayPlugins/com.google.play.billing/Samples/trivalKart/Assets/scripts/PlayerController.cs
@@ -1,72 +1,45 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityEngine.Serialization;
 
 public class PlayerController : MonoBehaviour
 {
 
-    public GameObject cam;
     public GameObject tapToDriveText;
     public Animator animator;
     [FormerlySerializedAs("MovementSpeed")] public float force = 6;
 
-    public GameObject gasStatus0;
-    public GameObject gasStatus1;
-    public GameObject gasStatus2;
-    public GameObject gasStatus3;
-    public GameObject gasStatus4;
-    public GameObject gasStatus5;
 
-    public GameObject noGasText;
-
-    private int _gasLevel;
-    private GameObject[] _gasStatus;
-    private int _fullGasLevel;
+    private Gas _gas;
     private Vector3 _carStartPos;
     private Rigidbody2D _rigidbody2D;
     private static readonly int Speed = Animator.StringToHash("speed");
-    
+
     public void Start()
-    { 
+    {
+        _gas = GetComponent<Gas>();
         _carStartPos = transform.position;
         _rigidbody2D = GetComponent<Rigidbody2D>();
-        _gasLevel = 5;
-        _fullGasLevel = 5;
-        _gasStatus = new GameObject[_fullGasLevel];
-        _gasStatus[0] = gasStatus0;
-        _gasStatus[1] = gasStatus1;
-        _gasStatus[2] = gasStatus2;
-        _gasStatus[3] = gasStatus3;
-        _gasStatus[4] = gasStatus4;
-        _gasStatus[5] = gasStatus5;
-        
     }
 
     // Update is called once per frame
     public void Update()
     {
-        
-        if (Input.GetMouseButtonUp(0))
-        {
-            if (_gasLevel > 0)
-            {
-                Drive();
-                _gasLevel--;
-                for (var i = 0; i < _fullGasLevel; i++)
-                {
-                    _gasStatus[i].SetActive(i == _gasLevel);
-                }
-            }
-            else
-            {
-                noGasText.SetActive(true);
-            }
-        }
         animator.SetFloat(Speed,  _rigidbody2D.velocity.magnitude);
+        
+    }
+
+    private void OnMouseDown()
+    {
+        if (_rigidbody2D.velocity.magnitude < 0.01 && _gas.ConsumeGas())
+        {
+            Drive();
+        }
     }
 
     private void FixedUpdate()
     {
-      if (cam.transform.position.x >= 29)
+      if (transform.position.x >= 25)
       {
           transform.position = _carStartPos;
       }
